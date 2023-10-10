0bde3988b03b41eaafc2335e03a423fad22d24d5
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-11 05:02:08 +0530

    Update config.py

--- Yone/config.py
+++ Yone/config.py
@@ -22,7 +22,7 @@ class Config(object):
     BOT_NAME = "Kora"
     # 
     # DATABASE_URL = "postgres://ixweewbx:9OoB_feF6d6wK1W4YycgwHzRHQXezsNA@arjuna.db.elephantsql.com/ixweewbx"  # sql
-    DATABASE_URL = "postgres://juspjans:X37Adum_spIMIswrhHTfHK9-JAEmk1cT@bubble.db.elephantsql.com/juspjans"  # sql
+    DATABASE_URL = "postgres://fnmtwspw:lAwoTHyHbfkAGJJ_BX8rxZw9h7mGtJUF@peanut.db.elephantsql.com/fnmtwspw"  # sql
     LOAD = []
     NO_LOAD = []
     WEBHOOK = False
