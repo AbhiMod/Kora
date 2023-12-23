d1009782d26074e86b162be363a70ced9629495a
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-23 15:45:29 +0530

    Update __init__.py

--- Yone/__init__.py
+++ Yone/__init__.py
@@ -29,7 +29,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    TOKEN = os.environ.get("TOKEN", "2027757649:AAEhgug3-fv3K3lIqud85BR01PFA6sjRBew")
+    TOKEN = os.environ.get("TOKEN", "2027757649:AAFN2JBG-gkaC_-_PnU1sTtM40RFzeFs9ME")
 
     try:
         OWNER_ID = int(os.environ.get("OWNER_ID", "2105971379"))
