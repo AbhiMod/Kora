929d0674dfb7e011cae9cf0de2b37a5df5001ff7
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-03 16:30:37 +0530

    Update config.py

--- Yone/config.py
+++ Yone/config.py
@@ -24,7 +24,7 @@ class Config(object):
     BOT_NAME = "Kora"
     # 
     # DATABASE_URL = "postgres://ixweewbx:9OoB_feF6d6wK1W4YycgwHzRHQXezsNA@arjuna.db.elephantsql.com/ixweewbx"  # sql
-    DATABASE_URL = "postgres://cbtysxae:AxhFIfnAH0KCMVtOwAUE25shztdqb2P9@peanut.db.elephantsql.com/cbtysxae"  # sql
+    DATABASE_URL = "postgres://fnmtwspw:lAwoTHyHbfkAGJJ_BX8rxZw9h7mGtJUF@peanut.db.elephantsql.com/fnmtwspw"  # sql
     LOAD = []
     NO_LOAD = []
     WEBHOOK = False
